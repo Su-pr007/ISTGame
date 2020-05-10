5916724fcb6bb6b9ff285e2983227800a99a22b4
Su-pr007 <maksimka.suvorov.01@list.ru>
2020-05-10 14:00:46 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace FirstGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MoveBgTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // MoveBgTimer
+            // 
+            this.MoveBgTimer.Enabled = true;
+            this.MoveBgTimer.Interval = 10;
+            this.MoveBgTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FirstGame.Properties.Resources.background_01;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "Form1";
+            this.Text = "2D Redemption";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MoveBgTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstGame
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] cloud;
+        int backgroundSpeed;
+        Random rand;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            for(int i = 0; i < cloud.Length; i++)
+            {
+                cloud[i].Left += backgroundSpeed;
+                if (cloud[i].Left >= 1280)
+                {
+                    cloud[i].Left = cloud[i].Height;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundSpeed = 5;
+            cloud = new PictureBox[20];
+            rand = new Random();
+
+            for(int i = 0; i < cloud.Length; i++)
+            {
+                cloud[i] = new PictureBox();
+                cloud[i].BorderStyle = BorderStyle.None;
+                cloud[i].Location = new Point(rand.Next(-1000, 1280), rand.Next(140, 380));
+                if (i % 2 == 1)
+                {
+                    cloud[i].Size = new Size(rand.Next(100, 225), rand.Next(30, 70));
+                    cloud[i].BackColor = Color.FromArgb(rand.Next(50, 125), 255, 205, 205);
+                }
+                else
+                {
+                    cloud[i].Size = new Size(150, 25);
+                    cloud[i].BackColor = Color.FromArgb(rand.Next(50, 125), 255, 205, 205);
+                }
+                this.Controls.Add(cloud[i]);
+            }
+        }
+    }
+}
